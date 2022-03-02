1133bbed6a2a21ceb00e318693844af0ce144e49
dulnithLiyanage <dulnithliyanage2@gmail.com>
2022-03-02 05:35:28 +0530

    removed bundles and went with default webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,10 @@ const path = require("path");
 module.exports = {
   entry: {
     index: "./src/index.js",
-    navbar: "./src/navbar.js",
-    home: "./src/home.js",
-    menu: "./src/menu.js",
-    contact: "./src/contact.js",
   },
   output: {
-    filename: "[name].bundle.js",
+    filename: "main.js",
     path: path.resolve(__dirname, "./dist"),
   },
-  mode: "none",
+  mode: "production",
 };
