dfbf27b8dd0763520237ef9b8b1a7317cb597712
dulnithLiyanage <dulnithliyanage2@gmail.com>
2022-03-02 17:52:36 +0530

    added styles to the home page

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,16 +1,24 @@
 import styles from "../styles.css";
+import background from "../images/background.jpg";
 
 const renderHomePage = () => {
+  const restaurnatName = "Polsambolaya";
+
   const content = document.getElementById("content");
 
   content.innerHTML = "";
 
   const title = document.createElement("h1");
-  title.innerText = "Home";
+  const caption = document.createElement("p");
+
+  title.innerText = restaurnatName;
+  caption.innerText = "A modern restaurant for the traditional lover";
 
   title.classList.add("title");
+  caption.classList.add("caption");
 
   content.appendChild(title);
+  content.appendChild(caption);
 };
 
 export default renderHomePage;
