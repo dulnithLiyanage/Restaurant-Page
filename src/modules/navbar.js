46dcd57257fac87a9af9cdcc929c762a05b78a69
dulnithLiyanage <dulnithliyanage2@gmail.com>
2022-03-05 18:54:05 +0530

    Added styles to navbar and buttons

--- src/modules/navbar.js
+++ src/modules/navbar.js
@@ -1,11 +1,15 @@
+import styles from "../styles.css";
+
 const renderNavbar = () => {
   const navbar = document.createElement("nav");
+  navbar.classList.add("navbar");
 
   const links = ["Home", "Menu", "Contact"];
 
   links.forEach((link) => {
     const button = document.createElement("button");
     button.textContent = link;
+    button.classList.add("button");
     navbar.appendChild(button);
   });
 
